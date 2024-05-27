f96dfb53a4b5dcb4ae0b0bf3b7b7d2fb8dd617d1
Parameswari Bala <parameswaribala@gmail.com>
2024-05-27 11:07:44 +0530

    updated

---
+++ day1/basics/prelimtypes.ts
@@ -0,0 +1,12 @@
+//variables
+import {Gender} from "./gender";
+
+let accountNo:number;
+let firstName:string;
+let lastName:string;
+let address:object;
+let contactNo:number;
+let email:string;
+let password:string;
+let gender:Gender;
+
