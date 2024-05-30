a33921902dbca9b788673f3f463bce4a09a137bf
Parameswari Bala <parameswaribala@gmail.com>
2024-05-30 13:02:34 +0530

    updated

--- projects/customerapi/src/api/routes/index.ts
+++ projects/customerapi/src/api/routes/index.ts
@@ -1,8 +1,10 @@
 import {Application} from "express";
 import customerRoute from "./customer.route";
+import corporateRoutes from "./corporate.routes";
 
 export class Routes{
     constructor(app:Application) {
         app.use("/api/customers",customerRoute)
+        app.use("/api/corporates",corporateRoutes)
     }
 }
--- projects/customerapi/src/config.ts
+++ projects/customerapi/src/config.ts
@@ -9,6 +9,15 @@ export const config = {
         min: 0,
         acquire: 30000,
         idle: 10000
+    },
+    aws_table_name: 'corporate',
+    aws_local_config: {
+        //Provide details for local configuration
+    },
+    aws_remote_config: {
+        accessKeyId: '',
+        secretAccessKey: '',
+        region: 'us-east-1',
     }
 };
 
