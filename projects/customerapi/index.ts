c3f0775f32f9621e9f48155d8e32acecd35aa7e4
Parameswari Bala <parameswaribala@gmail.com>
2024-05-30 15:46:13 +0530

    updated

--- projects/customerapi/index.ts
+++ projects/customerapi/index.ts
@@ -7,7 +7,7 @@ const server: Server = new Server(app);
 //const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3200;
 const PORT=3200;
 app
-    .listen(PORT, "localhost", function () {
+    .listen(PORT,  function () {
         console.log(`Server is running on port ${PORT}.`);
     })
     .on("error", (err: any) => {
