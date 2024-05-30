c2258c865a0432d2934ba6cd20a374243315492c
Parameswari Bala <parameswaribala@gmail.com>
2024-05-30 12:11:36 +0530

    updated

--- projects/accountapi/src/api/routes/index.ts
+++ projects/accountapi/src/api/routes/index.ts
@@ -3,6 +3,6 @@ import accountRoute from "./account.route";
 
 export class Routes{
     constructor(app:Application) {
-        app.use("/api/customers",accountRoute)
+        app.use("/api/accounts",accountRoute)
     }
 }
